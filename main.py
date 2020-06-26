5cfe79ed12219da3bf6e98e17e0c297fb6823c6e
Hodaya Maskin <t-homask@microsoft.com>
2020-06-26 15:18:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pymongo
+
+#Connect to the DB
+myclient = pymongo.MongoClient("mongodb://193.106.55.98:5000/")
+
+#use DB
+mydb = myclient["RecipeForMe"]
+#specific collection name
+mycol = mydb["ingredients"]
+#Create a dictionary and add to the Collection
+mydict = {}#{ "name": "John", "address": "Highway 37" }
+x = mycol.insert_one(mydict)
+y = mycol.find_one()
+print("hello")
