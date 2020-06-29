5979887f8643029a329668c411bb0850a757007b
mmalca <mmalca7@gmail.com>
2020-06-29 19:03:01 +0300

    added db folder and import

--- http_server/server_init.py
+++ http_server/server_init.py
@@ -1,5 +1,6 @@
 __author__ = 'mmalca'
 
+
 def init_server():
     PORT = 80
     #IP =
@@ -7,5 +8,3 @@ def init_server():
     server = HTTPServer(('10.10.248.98', PORT), http_handler)
     print('Server is running on port %s'%PORT)
     server.serve_forever()
-
-init_server()
--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import pymongo
+from http_server import server_init
+from http_server.server_init import *
 
-#Connect to the DB
-myclient = pymongo.MongoClient("mongodb://193.106.55.98:5000/")
 
-#use DB
-mydb = myclient["RecipeForMe"]
-#specific collection name
-mycol = mydb["ingredients"]
-#Create a dictionary and add to the Collection
-mydict = {}#{ "name": "John", "address": "Highway 37" }
-x = mycol.insert_one(mydict)
-y = mycol.find_one()
-print("hello world - commit 2")
-print("hello world - commit 3")
+def main():
+    init_server()
+if __name__ == "__main__":
+    main()
