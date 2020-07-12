837a2363eac850f4c16ffd18fc22eb11562dfaa5
Hodaya Maskin <t-homask@microsoft.com>
2020-07-12 22:34:18 +0300

    Add json serializtion and deserialization functionality

--- http_server/JsonOperations.py
+++ http_server/JsonOperations.py
@@ -1,13 +1,21 @@
-class Payload(object):
-    def __init__(self, action, method, data):
-        self.action = action
-        self.method = method
-        self.data = data
-
-message = {"action":"print","method":"onData","data":"Madan Mohan"}
 import json
 
-def as_payload(dct):
-    return Payload(dct['action'], dct['method'], dct['data'])
 
-payload = json.loads(message, object_hook = as_payload)
+class JsonToObject(object):
+    def __init__(self, j):
+        self.__dict__ = json.loads(j)
+
+
+class ObjectToJson(object):
+    def __init__(self, obj):
+        json.dumps(obj.__dict__)
+
+
+class Recipe:
+    def __init__(self, name, id):
+        self.name = name
+        self.processor = id
+
+    def ObjectToJson(self):
+        str = json.dumps(self.__dict__)
+        return str
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pymongo
 from http_server.server_init import init_server
-#from http_server.JsonOperations import *
+from http_server.JsonOperations import *
 from FastTextOperations.first_impression import *
+
+# my tries to serialize / deserialize from/to json
+j = '{"action": "print", "method": "onData", "data": "Madan Mohan"}'
+p = JsonToObject(j)
+
+r = Recipe('hodaya', 123)
+m = r.ObjectToJson()
+print(m)
+# end of json tests
 
 first_try()
 #Connect to the DB
